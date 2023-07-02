d04cfd312a293318b6337077a5914b12a223eb6a
zanjie1999 <zanjie1999@yeah.net>
2023-07-02 18:59:51 +0800

    2023

--- httpme.go
+++ httpme.go
@@ -1,4 +1,4 @@
-/* Copyright（2） 2018 by  zanjie1999 .
+/* Copyright（2） 2023 by  zanjie1999 .
 Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
